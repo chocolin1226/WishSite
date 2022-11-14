1cb84c5f8bcf704c3c755bbbf9ea4aa811e066da
chocolin <h9711022@gmail.com>
2022-11-14 17:06:13 +0800

    create wish_lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get "/", to: "pages#home" 簡化如下
+  root "pages#home"
+  get "/about", to: "pages#about"
+  get "/contact", to: "pages#contact"
 end
