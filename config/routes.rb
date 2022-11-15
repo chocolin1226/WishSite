51f8604f0a4e41acded71d3c40206b6d72f42c30
chocolin <h9711022@gmail.com>
2022-11-15 17:42:09 +0800

    cRud WishList

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  # get "/", to: "pages#home" 簡化如下
-  root "pages#home"
+  
   get "/about", to: "pages#about"
   get "/contact", to: "pages#contact"
+
+  get "/make_a_wish", to: "wish_list#card"
+  get "/new_wish", to: "wish_list#new_wish"
+  post "/create_wish", to: "wish_list#create_wish"
+  get "/wish_card_info/:id", to: "wish_list#show_wish", as: "wish_card_info"
+
+  root "wish_list#card"
+
 end
