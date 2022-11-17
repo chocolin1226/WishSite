155891b650d372abb3a4f12c917521f2c449cd13
chocolin <h9711022@gmail.com>
2022-11-17 17:00:28 +0800

    crud WishList

---
+++ app/controllers/wish_lists_controller.rb
@@ -0,0 +1,49 @@
+class WishListsController < ApplicationController
+  before_action :find_wish_list, only: [:edit, :update, :destroy, :show]
+
+  def index
+    @wish_lists = WishList.all
+  end
+
+  def new
+    @wish_list = WishList.new
+  end
+
+  def create
+    @wish_list = WishList.new(wish_list_params)
+
+    if @wish_list.save
+      redirect_to root_path, notice: "成功新增許願卡！"
+    else
+      render :new 
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update 
+    if @wish_list.update(wish_list_params)
+      redirect_to root_path, notice: "成功更新許願卡！"
+    else
+      render :edit 
+    end
+  end
+  
+  def destroy
+    @wish_list.delete
+    redirect_to root_path, notice: "成功刪除許願卡！"
+  end
+
+  private
+    def find_wish_list
+      @wish_list = WishList.find(params[:id])
+    end
+
+    def wish_list_params
+      params.require(:wish_list).permit(:title, :description)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,8 @@ Rails.application.routes.draw do
   get "/about", to: "pages#about"
   get "/contact", to: "pages#contact"
 
-  get "/make_a_wish", to: "wish_list#card"
-  get "/new_wish", to: "wish_list#new_wish"
-  post "/create_wish", to: "wish_list#create_wish"
-  get "/wish_card_info/:id", to: "wish_list#show_wish", as: "wish_card_info"
+  resources :wish_lists
 
-  root "wish_list#card"
+  root "wish_lists#index"
 
 end
